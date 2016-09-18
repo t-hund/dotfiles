1c5f3032309a2e94bf151325e6c86ee18eedb517
Tommy Vagbratt <tommy@vagbratt.se>
2016-09-18 22:26:49 +0200

    Initial bash aliases

---
+++ bash/aliases.bash
@@ -0,0 +1,11 @@
+#=======================================================================
+# Bash aliases
+#=======================================================================
+
+# Abbreviations and shortcuts
+alias q="exit"
+alias Q="exit"
+alias e="$EDITOR"
+alias v="view"
+alias g="git"
+alias j="z"
