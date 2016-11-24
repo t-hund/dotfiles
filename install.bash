bce751e33c19c7dc4c19f0ce6d288e0a8c0d0c3a
Tommy Vagbratt <tommy@vagbratt.se>
2016-11-24 11:44:44 +0100

    Setup dummy script for installing dependencies

--- install.bash
+++ install.bash
@@ -6,3 +6,6 @@ DOTFILES=$HOME/dotfiles
 
 # Git
 ln -sf $DOTFILES/git/config.git $HOME/.gitconfig
+
+## Install dependencies
+#source $DOTFILES/install_dependencies.bash
---
+++ install_dependencies.bash
@@ -0,0 +1,7 @@
+#=======================================================================
+# Install dependencies
+#=======================================================================
+
+# Depenedies
+# cat with beautiful colors. requires Pygments installed! use pip or
+sudo apt-get install python-pygments
