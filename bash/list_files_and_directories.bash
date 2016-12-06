224549380a5e484136ce6fcdb05e337305d09179
Tommy Vagbratt <tommy@vagbratt.se>
2016-12-06 13:35:27 +0100

    Print sizes in human readable format

--- bash/list_files_and_directories.bash
+++ bash/list_files_and_directories.bash
@@ -17,7 +17,7 @@ export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40
 alias l="ls -lF ${colorflag}"
 
 # List all files in long format, including dotfiles
-alias la="ls -laF ${colorflag}"
+alias la="ls -lahF ${colorflag}"
 
 # List directories in long format
-alias ld="ls -lF ${colorflag} | grep --color=never '^d'"
+alias ld="ls -lhF ${colorflag} | grep --color=never '^d'"
