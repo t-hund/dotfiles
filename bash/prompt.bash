547e5477633543ba178508464bf7271ac106c6f7
Tommy Vagbratt <tommy@vagbratt.se>
2016-12-27 22:57:18 +0100

    Display current working directory in terminal title

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -2,7 +2,11 @@
 # Bash prompt
 #=======================================================================
 
+# Display current working directory in terminal title
+TERMINAL_TITLE="\w"
+PS1="\033]0;$TERMINAL_TITLE\007"
+
 # Basic prompt
-PS1="\n\u at \h in \w\n$ "
+PS1+="\n\u at \h in \w\n$ "
 
 export PS1
