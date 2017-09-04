8ef3a5533478fb879dcf55519c485c98ae3b4b1b
Tommy Vagbratt <tommy@vagbratt.se>
2017-09-04 03:12:23 +0200

    Don't stray from the path

---
+++ bash/path.bash
@@ -0,0 +1,12 @@
+#=======================================================================
+# Path
+#=======================================================================
+
+export DOTFILES="$HOME/dotfiles"
+export GOPATH="$HOME/Dropbox/dev/go"
+export PATH="$GOPATH/bin:$PATH";
+export PATH="$HOME/bin:$PATH";
+export PATH="$HOME/uu/compiler_project/uc/bin/:$PATH";
+export PATH="$HOME/projects/nice/prototype:$PATH";
+export PATH="$HOME/Dropbox/code/encore/encore/release:$PATH";
+export GTEST_HOME="$HOME/Dropbox/programming/libs/googletest/googletest"
