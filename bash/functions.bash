57ae9d2f5a5b59e95e5be2cc2a6605f21265049f
Tommy Vagbratt <tommy@vagbratt.se>
2017-09-16 00:29:34 +0200

    Use git's colored diff when available

--- bash/functions.bash
+++ bash/functions.bash
@@ -15,3 +15,12 @@ function fs() {
         du $arg .[^.]* *;
     fi;
 }
+
+# Use Git’s colored diff when available
+hash git &>/dev/null;
+if [ $? -eq 0 ]; then
+    function diff() {
+        git diff --no-index --color-words "$@";
+    }
+fi;
+
