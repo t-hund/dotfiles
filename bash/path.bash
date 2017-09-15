c9d585fef95647c0a1a1f9b9e5c841fcf633c36c
Tommy Vagbratt <tommy@vagbratt.se>
2017-09-15 18:41:43 +0200

    Reformat path.bash

--- bash/path.bash
+++ bash/path.bash
@@ -4,9 +4,10 @@
 
 export DOTFILES="$HOME/dotfiles"
 export GOPATH="$HOME/Dropbox/dev/go"
+export GTEST_HOME="$HOME/Dropbox/programming/libs/googletest/googletest"
+
 export PATH="$GOPATH/bin:$PATH";
 export PATH="$HOME/bin:$PATH";
 export PATH="$HOME/uu/compiler_project/uc/bin/:$PATH";
 export PATH="$HOME/projects/nice/prototype:$PATH";
 export PATH="$HOME/Dropbox/code/encore/encore/release:$PATH";
-export GTEST_HOME="$HOME/Dropbox/programming/libs/googletest/googletest"
