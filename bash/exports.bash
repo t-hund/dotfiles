7eda74f7008501a0639639c7cdb6592d6df06408
Tommy Vagbratt <tommy@vagbratt.se>
2017-09-15 17:39:13 +0200

    Use vim as man pager

--- bash/exports.bash
+++ bash/exports.bash
@@ -4,3 +4,6 @@
 
 # Make vim the default editor
 export EDITOR="vim";
+
+# Don’t clear the screen after quitting a manual page
+export MANPAGER="/bin/sh -c \"col -b | vim -c 'set ft=man ts=8 nomod nolist nonu noma' -\""
