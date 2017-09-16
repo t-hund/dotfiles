a5dac8551c7300758c16a60445b3ab0963c7dee4
Tommy Vagbratt <tommy@vagbratt.se>
2017-09-16 17:01:55 +0200

    Autocomplete for common bash commands!

---
+++ bash/autocomplete.bash
@@ -0,0 +1,9 @@
+#=======================================================================
+# Autocomplete
+#=======================================================================
+
+# Add tab completion for many Bash commands
+if [ -f /etc/bash_completion ]; then
+    source /etc/bash_completion;
+fi;
+
