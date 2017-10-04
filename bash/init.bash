25893c458ea963e24ca346f678b5fd9478979793
Tommy Vagbratt <tommy@vagbratt.se>
2017-10-04 20:01:49 +0200

    Symlink bashrc and bash_profile to initialize bash

---
+++ bash/init.bash
@@ -0,0 +1,18 @@
+#=======================================================================
+# Bash init
+#=======================================================================
+
+export DOTFILES="$HOME/Dropbox/dotfiles"
+
+source "$DOTFILES/bash/path.bash"
+source "$DOTFILES/bash/functions.bash"
+source "$DOTFILES/bash/exports.bash"
+source "$DOTFILES/bash/aliases.bash"
+source "$DOTFILES/bash/prompt.bash"
+source "$DOTFILES/bash/options.bash"
+source "$DOTFILES/bash/autocomplete.bash"
+source "$DOTFILES/bash/version_managers.bash"
+
+
+# initialize z   (https://github.com/rupa/z)
+source "$DOTFILES/bin/z.sh"
--- install.bash
+++ install.bash
@@ -7,3 +7,6 @@ DOTFILES=$HOME/Dropbox/dotfiles
 # Git
 ln -sf $DOTFILES/git/config.git $HOME/.gitconfig
 
+# Bash
+ln -sf $DOTFILES/bash/init.bash $HOME/.bashrc
+ln -sf $DOTFILES/bash/init.bash $HOME/.bash_profile
