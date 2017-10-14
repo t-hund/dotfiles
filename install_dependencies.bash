904cc96b07aa2ffa72f8932f6da0b2d2bad6d4ac
Tommy Vagbratt <tommy@vagbratt.se>
2017-10-14 23:27:34 +0200

    Add more install dependencies

--- install_dependencies.bash
+++ install_dependencies.bash
@@ -2,6 +2,43 @@
 # Install dependencies
 #=======================================================================
 
-# Depenedies
-# cat with beautiful colors. requires Pygments installed! use pip or
-sudo apt-get install python-pygments
+# Vim
+sudo apt install vim-gtk3
+
+# Latex and pdfs
+sudo apt install texlive-full --yes
+sudo apt install zathura --yes
+sudo apt install pandoc --yes
+sudo apt install pdftk --yes
+
+# Build tools
+sudo apt install build-essential --yes
+sudo apt install make --yes
+sudo apt install cmake --yes
+
+# C and C++ compilers
+sudo apt install clang --yes
+sudo apt install gcc --yes
+sudo apt instll g++ --yes
+
+# Debuggers
+sudo apt install lldb --yes
+sudo apt install gdb --yes
+sudo apt install valgrind --yes
+
+# Documentation
+sudo apt install doxygen --yes
+
+# Haskell
+sudo apt install ghc --yes
+sudo apt install haskell-stack --yes
+sudo apt install cabal-install --yes
+
+# Utils
+sudo apt install silversearcher-ag --yes
+sudo apt install python-pygments --yes
+sudo apt install screen --yes
+sudo apt install tmux --yes
+sudo apt install tree --yes
+sudo apt install htop --yes
+sudo apt install curl --yes
