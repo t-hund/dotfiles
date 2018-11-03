885ca13ef482441fa9d93e08541cc168f1fa4e64
Tommy Vagbratt <tommy@vagbratt.se>
2018-11-03 22:00:07 +0100

    Make it possible to start Ranger with Rofi

--- applications/install.bash
+++ applications/install.bash
@@ -6,3 +6,4 @@ sudo cp lock.desktop /usr/share/applications/
 sudo cp logout.desktop /usr/share/applications/
 sudo cp reboot.desktop /usr/share/applications/
 sudo cp shutdown.desktop /usr/share/applications/
+sudo cp ranger.desktop /usr/share/applications/
