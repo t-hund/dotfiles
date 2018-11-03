3edb2fda18ea2b651280a32ba2017e5362964f06
Tommy Vagbratt <tommy@vagbratt.se>
2018-11-03 21:55:34 +0100

    Use Rofi to lock, reboot, shutdown, and log out.

---
+++ applications/install.bash
@@ -0,0 +1,8 @@
+#=======================================================================
+# Install
+#=======================================================================
+
+sudo cp lock.desktop /usr/share/applications/
+sudo cp logout.desktop /usr/share/applications/
+sudo cp reboot.desktop /usr/share/applications/
+sudo cp shutdown.desktop /usr/share/applications/
