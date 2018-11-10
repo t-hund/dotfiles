08125e1c8a6dd6d2b5500e3dbec21dab055a7ab1
Tommy Vagbratt <tommy@vagbratt.se>
2018-11-10 16:36:21 +0100

    Move nvm initialization from startup
    By wrapping the initialization of nvm into a function it can be loaded
    when needed and not slow down every shell startup.

--- bash/version_managers.bash
+++ bash/version_managers.bash
@@ -7,6 +7,7 @@
 # export PATH="$PYENV_ROOT/bin:$PATH"
 # eval "$(pyenv init -)"
 
+
 # Ruby
 if [[ -s "/usr/local/share/chruby/chruby.sh" ]]; then
     source /usr/local/share/chruby/chruby.sh
@@ -14,8 +15,11 @@ if [[ -s "/usr/local/share/chruby/chruby.sh" ]]; then
     chruby 2.4
 fi
 
-# Load nvm
-# WHY SO SLOOOOOOWWWLYYY?!
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Call init_nvm to initialize nvm. To me, nvm is too slow to run during each shell
+# startup.
+init_nvm() {
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+}
