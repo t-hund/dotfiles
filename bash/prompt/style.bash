274012bd2fa32fee79758da43e30c63263a88c7a
Tommy Vagbratt <tommy@vagbratt.se>
2018-12-04 19:21:55 +0100

    Minor prompt style improvement

--- bash/prompt/style.bash
+++ bash/prompt/style.bash
@@ -5,7 +5,7 @@
 # Basic
 PROMPT_STYLE_RESET="$(tput sgr0)"
 PROMPT_STYLE_BOLD="$(tput bold)"
-PROMPT_STYLE_USER="$(tput setaf 66)"
+PROMPT_STYLE_USER="$(tput setaf 30)"
 PROMPT_STYLE_ROOT="$(tput setaf 196)"
 PROMPT_STYLE_HOST="$(tput setaf 131)"
 PROMPT_STYLE_DIRECTORY="$(tput setaf 178)"
@@ -14,7 +14,7 @@ PROMPT_STYLE_SYMBOL="$(tput setaf 240)"
 PROMPT_STYLE_COMMAND="$(tput setaf 255)"
 
 # Git
-PROMPT_STYLE_GIT_BRANCH="$(tput setaf 66)"
+PROMPT_STYLE_GIT_BRANCH="$(tput setaf 30)"
 PROMPT_STYLE_GIT_STATUS="$(tput setaf 166)"
 PROMPT_STYLE_GIT_STATUS_STAGED="$(tput setaf 166)"
 PROMPT_STYLE_GIT_STATUS_MODIFIED="$(tput setaf 166)"
